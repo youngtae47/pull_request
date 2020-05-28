8a39ca9ba948cb47390939bd9adf676d84c48cf6
youngtae <youngtea93@gmail.com>
2020-05-28 19:23:22 +0900

    add insertion sort

--- test.c
+++ test.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void i_sort(int *d, int idx1, int idx2){
+    int i, j=0;
+    for(i=idx1+1 ; i<idx2+1 ; i++){
+        int tmp = d[i];
+        for(j=i-1 ; j>=0 && d[j]>tmp; j--)
+            d[j+1] = d[j];
+
+        d[j+1] =tmp;
+    }
+}
+
 int main(int argc, char **argv){
 
     int n,*d;
@@ -8,6 +19,8 @@ int main(int argc, char **argv){
     int e = fread(&n, sizeof(int), 1, input);
     d = malloc(sizeof(int)*n);
     e = fread(d, sizeof(int), n, input);
+
+    i_sort(d, 0, n-1);
 
     fclose(input);
     free(d);
